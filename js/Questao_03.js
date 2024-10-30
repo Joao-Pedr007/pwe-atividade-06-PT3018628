1b2e9b8c014140a01e0729580de48d03fc27b1bc
Joao-Pedr007 <160731637+Joao-Pedr007@users.noreply.github.com>
2024-10-30 09:23:46 -0300

    Update Questao_03.js

--- js/Questao_03.js
+++ js/Questao_03.js
@@ -1,6 +1,6 @@
 var nomeAluno = "João Pedro da Silva";
-var nota_01 = 10;
-var nota_02 = 10;
+var nota_01 = 7;
+var nota_02 = 8.5;
 var nota_01_peso = nota_01 * 0.6;
 var nota_02_peso = nota_02 * 0.4;
 var nota_final = nota_01_peso + nota_02_peso;
